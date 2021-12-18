1f1a8b7e4c3e9303238d85ac62849383e490924f
BartoszBartt <bartoszewski@student.agh.edu.pl>
2021-12-18 14:54:08 +0100

    druga funkcja, dodawanie id do półproduktu, jeśli ona nie ma takowego

--- src/package.cpp
+++ src/package.cpp
@@ -7,8 +7,8 @@ void if_exist_then_erase(std::set<ElementID>& search, ElementID erase){
 }
 
 //tutaj trzeba będzie zmienić funkcję tak, że będzie wstawiałą id, jeśli dana wartość nie istnieje
-//void if_exist_then_erase(std::set<ElementID>& search, ElementID erase){
-//    if(search.find(erase) != search.end()){
-//        search.insert(erase); //wstawia do kontenera search, wartość eraase jeśli nie znajdzie tej wartości
-//    }
-//}
+void if_not_exist_then_insert(std::set<ElementID>& search, ElementID erase){
+    if(search.find(erase) == search.end()){
+        search.insert(erase);
+    }
+}
