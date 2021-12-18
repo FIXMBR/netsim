64d18fa6cb28a415827917d4cd55c40fe495f4de
BartoszBartt <bartoszewski@student.agh.edu.pl>
2021-12-18 14:39:23 +0100

    comm

--- include/types.hpp
+++ include/types.hpp
@@ -1,7 +1,7 @@
 #ifndef NETSIM_TYPES_HPP
 #define NETSIM_TYPES_HPP
 
-using ElementID = unsigned int;
+using ElementID = unsigned int; //id jest tylko liczbą całkowitą dodatnią
 //tutaj trzeba będzie dodawać typy, któych będziemy używać w programie
 
 #endif //NETSIM_TYPES_HPP
