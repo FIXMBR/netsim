b1bf8c7949b25f50704d6fe81c79526f61e8a90e
BartoszBartt <bartoszewski@student.agh.edu.pl>
2021-12-19 20:35:09 +0100

    Dołożone funkcjie natalii

--- include/package.hpp
+++ include/package.hpp
@@ -6,19 +6,18 @@
 #include <set>
 #include "types.hpp"
 
-using id_t = int; //typ ElementID
 
 class Package{
 private:
     static std::set<ElementID> assigned_IDs; //przechowywać będzie przydzielone obecnie ID
     static std::set<ElementID> freed_IDs; //przechowywać będzie ID kiedyś użyte, ale obecnie zwolnione
+    ElementID max_id = 0;
+    ElementID id_ = max_id;
 
 public:
     Package();
-//    Package(id_t i = 0): ElementID(i) {}; //trzeba zmienić w zależności od sposobu wyznaczania ID
-    Package(Package &&) noexcept;
     Package(const Package&) = delete;
-    id_t get_id() const; //czy tutaj trzeba to
+    ElementID get_id();
     ~Package();
 };
 
--- src/package.cpp
+++ src/package.cpp
@@ -1,7 +1,22 @@
 #include "package.hpp"
 
+std::set<ElementID> Package::assigned_IDs;
+std::set<ElementID> Package::freed_IDs;
+
+//zwraca nowe id
+ElementID Package::get_id(){
+    if(freed_IDs.empty()){
+
+        return max_id += 1;
+    }
+    else {
+        return *Package::freed_IDs.begin();
+    }
+}
+
+
 void if_exist_then_erase(std::set<ElementID>& search, ElementID erase){
-    if(search.find(erase) != search.end()){
+    if(search.find(erase) != search.end()) {
         search.erase(erase); //wstawia do kontenera search, wartość eraase jeśli nie znajdzie tej wartości
     }
 }
@@ -13,3 +28,11 @@ void if_not_exist_then_insert(std::set<ElementID>& search, ElementID erase){
     }
 }
 
+
+Package::~Package() {
+
+    if (id_ != max_id){
+        if_exist_then_erase(assigned_IDs, id_);
+        if_not_exist_then_insert(freed_IDs, id_);
+    }
+}
