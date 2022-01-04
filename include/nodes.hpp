5369057c37024595f5c87f333ccf1412ca90a1b1
FIXMBR <16748784+FIXMBR@users.noreply.github.com>
2022-01-04 16:39:28 +0100

    fix

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -2,7 +2,29 @@
 #ifndef NODES_HPP
 #define NODES_HPP
 
+#include <optional>
+#include "package.hpp"
+class ReceiverPreferences
+{
+public:
+    ReceiverPreferences(PropabilityGenerator pg);
+    void add_receiver(IPackageReceiver *r);
+    void remove_receiver(IPackageReceiver *r);
+    IPackageReceiver *choose_receiver();
+    preferences_t &get_preferences() const;
+};
 
+class PackageSender
+{
+private:
+public:
+    ReceiverPreferences receiver_preferences_;
+    PackageSender(PackageSender &&);
+    void send_package();
 
-#endif //NODES_HPP
-//1b: Bartoszewski (406690), Gajek (400365), Gąsior (407326), Kowalczyk (406185)
+protected:
+    std::optional<Package> &get_sending_buffer() const;
+};
+
+#endif //NODES_HPP \
+       //1b: Bartoszewski (406690), Gajek (400365), Gąsior (407326), Kowalczyk (406185)
--- include/types.hpp
+++ include/types.hpp
@@ -2,10 +2,14 @@
 #ifndef NETSIM_TYPES_HPP
 #define NETSIM_TYPES_HPP
 
+#include <functional>
+
 using ElementID = unsigned int; // id jest tylko liczbą całkowitą dodatnią
 // tutaj trzeba będzie dodawać typy, któych będziemy używać w programie
 
 using size_type = unsigned int;
 
-#endif // NETSIM_TYPES_HPP
-//1b: Bartoszewski (406690), Gajek (400365), Gąsior (407326), Kowalczyk (406185)
+using ProbabilityGenerator = std::function<double()>;
+
+#endif // NETSIM_TYPES_HPP \
+       //1b: Bartoszewski (406690), Gajek (400365), Gąsior (407326), Kowalczyk (406185)
