c6d5d9aed2c3b7e9eceaec570d5c634cd967de3f
BartoszBartt <bartoszewski@student.agh.edu.pl>
2022-01-08 14:25:49 +0100

    j

--- include/factory.hpp
+++ include/factory.hpp
@@ -2,6 +2,9 @@
 #ifndef NETSIM_FACTORY_HPP
 #define NETSIM_FACTORY_HPP
 
+#include "types.hpp"
+#include "nodes.hpp"
+
 
 #endif //NETSIM_FACTORY_HPP
 //1b: Bartoszewski (406690), Gajek (400365), Gąsior (407326), Kowalczyk (406185)
