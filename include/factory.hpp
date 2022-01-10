eb26bf973694fb979fa143aa60e6e226688e75b7
FIXMBR <16748784+FIXMBR@users.noreply.github.com>
2022-01-10 01:58:03 +0100

    kompiluje się śmieć w końcu

--- include/factory.hpp
+++ include/factory.hpp
@@ -5,7 +5,12 @@
 #include "types.hpp"
 #include "nodes.hpp"
 
-enum class NodeColor { UNVISITED, VISITED, VERIFIED };
+enum class NodeColor
+{
+    UNVISITED,
+    VISITED,
+    VERIFIED
+};
 
 template <class Node>
 class NodeCollection
@@ -18,17 +23,17 @@ public:
     void add(Node &&node) { collection_.push_back(std::move(node)); };
     void remove_by_id(ElementID id)
     {
-        collection_.remove_if([id](Node n) //TODO vibecheck czy to działa
+        collection_.remove_if([id](Node &n) //TODO vibecheck czy to działa
                               { return n.get_id() == id; });
     };
     NodeCollection<Node>::iterator find_by_id(ElementID id)
     {
-        return std::find_if(collection_.begin(), collection_.end(), [id](Node n)
+        return std::find_if(collection_.begin(), collection_.end(), [id](Node &n)
                             { return n.get_id() == id; });
     };
     NodeCollection<Node>::const_iterator find_by_id(ElementID id) const
     {
-        return std::find_if(collection_.cbegin(), collection_.cend(), [id](Node n)
+        return std::find_if(collection_.cbegin(), collection_.cend(), [id](Node const &n)
                             { return n.get_id() == id; });
     };
     const_iterator begin() const { return collection_.begin(); };
@@ -42,61 +47,64 @@ private:
     container_t collection_;
 };
 
-
-class Factory{
+class Factory
+{
 private:
     NodeCollection<Ramp> ramp_collection_;
     NodeCollection<Worker> worker_collection_;
     NodeCollection<Storehouse> storehouse_collection_;
-    template <class Node> void remove_receiver(NodeCollection<Node>& collection, ElementID id){
+    template <class Node>
+    void remove_receiver(NodeCollection<Node> &collection, ElementID id)
+    {
 
         typename NodeCollection<Node>::iterator receiver = collection.find_by_id(id);
-//        IPackageReceiver& receiver = collection.find_by_id(id);
+        //        IPackageReceiver& receiver = collection.find_by_id(id);
 
-        for (auto& node : worker_collection_) {
+        for (auto &node : worker_collection_)
+        {
             node.receiver_preferences_.remove_receiver(&*receiver);
         }
-        for (auto& node : ramp_collection_) {
+        for (auto &node : ramp_collection_)
+        {
             node.receiver_preferences_.remove_receiver(&*receiver);
         }
 
         collection.remove_by_id(id);
     }
-    bool has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors) const;
+    bool has_reachable_storehouse(const PackageSender *sender, std::map<const PackageSender *, NodeColor> &node_colors) const;
+
 public:
     NodeCollection<Ramp>::const_iterator ramp_cbegin() const { return ramp_collection_.begin(); }; // zwraca stały iterator na pierwszy element kontenera
-    NodeCollection<Ramp>::const_iterator ramp_cend() const { return ramp_collection_.cend(); };   // zwraca stały iterator na ostatni element kontenera
+    NodeCollection<Ramp>::const_iterator ramp_cend() const { return ramp_collection_.cend(); };    // zwraca stały iterator na ostatni element kontenera
     NodeCollection<Ramp>::const_iterator ramp_begin() const { return ramp_collection_.begin(); };  //
     NodeCollection<Ramp>::const_iterator ramp_end() const { return ramp_collection_.end(); };
 
-    void add_ramp(Ramp&& r){ramp_collection_.add(std::move(r));};
-    void remove_ramp(ElementID id){ramp_collection_.remove_by_id(id);};
-    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id) {return ramp_collection_.find_by_id(id); };
-    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id)const{return ramp_collection_.find_by_id(id); };
-
+    void add_ramp(Ramp &&r) { ramp_collection_.add(std::move(r)); };
+    void remove_ramp(ElementID id) { ramp_collection_.remove_by_id(id); };
+    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id) { return ramp_collection_.find_by_id(id); };
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id) const { return ramp_collection_.find_by_id(id); };
 
     NodeCollection<Storehouse>::const_iterator storehouse_cbegin() const { return storehouse_collection_.cbegin(); }; // zwraca stały iterator na pierwszy element kontenera
-    NodeCollection<Storehouse>::const_iterator storehouse_cend() const { return storehouse_collection_.cend(); };   // zwraca stały iterator na ostatni element kontenera
-    NodeCollection<Storehouse>::const_iterator storehouse_begin() const { return storehouse_collection_.begin(); };  //
+    NodeCollection<Storehouse>::const_iterator storehouse_cend() const { return storehouse_collection_.cend(); };     // zwraca stały iterator na ostatni element kontenera
+    NodeCollection<Storehouse>::const_iterator storehouse_begin() const { return storehouse_collection_.begin(); };   //
     NodeCollection<Storehouse>::const_iterator storehouse_end() const { return storehouse_collection_.end(); };
 
-    void add_storehouse(Storehouse&& s){storehouse_collection_.add(std::move(s));};
-//    void remove_storehouse(ElementID id){remove_receiver(storehouse_collection_,id);};
-    NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id) {return storehouse_collection_.find_by_id(id); };
-    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id)const{return storehouse_collection_.find_by_id(id); };
+    void add_storehouse(Storehouse &&s) { storehouse_collection_.add(std::move(s)); };
+    //    void remove_storehouse(ElementID id){remove_receiver(storehouse_collection_,id);};
+    NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id) { return storehouse_collection_.find_by_id(id); };
+    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const { return storehouse_collection_.find_by_id(id); };
 
     NodeCollection<Worker>::const_iterator worker_cbegin() const { return worker_collection_.cbegin(); }; // zwraca stały iterator na pierwszy element kontenera
-    NodeCollection<Worker>::const_iterator worker_cend() const { return worker_collection_.cend(); };   // zwraca stały iterator na ostatni element kontenera
-    NodeCollection<Worker>::const_iterator worker_begin() const { return worker_collection_.begin(); };  //
+    NodeCollection<Worker>::const_iterator worker_cend() const { return worker_collection_.cend(); };     // zwraca stały iterator na ostatni element kontenera
+    NodeCollection<Worker>::const_iterator worker_begin() const { return worker_collection_.begin(); };   //
     NodeCollection<Worker>::const_iterator worker_end() const { return worker_collection_.end(); };
 
-    void add_worker(Worker&& s){worker_collection_.add(std::move(s));};
-    void remove_worker(ElementID id){remove_receiver(worker_collection_,id);};
-    NodeCollection<Worker>::iterator find_worker_by_id(ElementID id) {return worker_collection_.find_by_id(id); };
-    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id)const{return worker_collection_.find_by_id(id); };
+    void add_worker(Worker &&s) { worker_collection_.add(std::move(s)); };
+    void remove_worker(ElementID id) { remove_receiver(worker_collection_, id); };
+    NodeCollection<Worker>::iterator find_worker_by_id(ElementID id) { return worker_collection_.find_by_id(id); };
+    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const { return worker_collection_.find_by_id(id); };
 
-
-    bool is_consistent(void) const ;//FIXME
+    bool is_consistent(void) const; //FIXME
     void do_deliveries(Time time);
     void do_package_passing(void);
     void do_work(Time time);
--- src/factory.cpp
+++ src/factory.cpp
@@ -1,6 +1,5 @@
 #include "factory.hpp"
 
-
 //template <class Node> void Factory::remove_receiver(NodeCollection<Node>& collection,  ElementID id){
 //
 //    typename NodeCollection<Node>::iterator receiver = collection.find_by_id(id);
@@ -16,75 +15,101 @@
 //    collection.remove_by_id(id);
 //}
 
-
 void Factory::do_work(Time time)
 {
-    std::for_each(worker_collection_.begin(), worker_collection_.end(), [time](auto elem){elem.do_work(time);});
+    std::for_each(worker_collection_.begin(), worker_collection_.end(), [time](Worker &elem)
+                  { elem.do_work(time); });
+    //   { elem.do_work(time); });
+    //   { dynamic_cast<Worker *>(elem)->do_work(time); });
 }
-
 
 void Factory::do_deliveries(Time time)
 {
-    std::for_each(ramp_collection_.begin(), ramp_collection_.end(), [time](auto elem){elem.deliver_goods(time);});
+    std::for_each(ramp_collection_.begin(), ramp_collection_.end(), [time](Ramp &elem)
+                  { elem.deliver_goods(time); });
 }
 
 void Factory::do_package_passing()
 {
-    std::for_each(worker_collection_.begin(), worker_collection_.end(), [](auto elem){elem.send_package(time);});
-    std::for_each(ramp_collection_.begin(), ramp_collection_.end(), [](auto elem){elem.send_package(time);});
+    std::for_each(worker_collection_.begin(), worker_collection_.end(), [](Worker &elem)
+                  { elem.send_package(); });
+    std::for_each(ramp_collection_.begin(), ramp_collection_.end(), [](Ramp &elem)
+                  { elem.send_package(); });
 }
 
+bool Factory::is_consistent() const
+{
+    std::map<const PackageSender *, NodeColor> color;
+    // std::for_each(worker_collection_.begin(), worker_collection_.end(), [&color](Worker &elem)
+    //               { color.insert(
+    //                     std::pair<const PackageSender *, NodeColor>(dynamic_cast<PackageSender *>(&elem), NodeColor::UNVISITED)); });
+    // std::for_each(ramp_collection_.begin(), ramp_collection_.end(), [&color](Ramp &elem)
+    //               { color.insert(
+    //                     std::pair<const PackageSender *, NodeColor>(dynamic_cast<PackageSender *>(&elem), NodeColor::UNVISITED)); });
 
+    for (NodeCollection<Worker>::const_iterator it = worker_collection_.begin(); it != worker_collection_.end(); it++)
+    {
+        color.insert(std::pair<const PackageSender *, NodeColor>(dynamic_cast<const PackageSender *>(&*it), NodeColor::UNVISITED));
+    }
 
-bool Factory::is_consistent() const {
-    std::map<PackageSender*,NodeColor> color;
-    std::for_each(worker_collection_.begin(), worker_collection_.end(), [color](auto elem){color.insert({elem,NodeColor::UNVISITED});});
-    std::for_each(storehouse_collection_.begin(), storehouse_collection_.end(), [color](auto elem){color.insert({elem,NodeColor::UNVISITED});});
+    for (NodeCollection<Ramp>::const_iterator it = ramp_collection_.begin(); it != ramp_collection_.end(); it++)
+    {
+        color.insert(std::pair<const PackageSender *, NodeColor>(dynamic_cast<const PackageSender *>(&*it), NodeColor::UNVISITED));
+    }
 
-//    NodeCollection<Ramp>::iterator it;
-    for (auto& ramp : ramp_collection_) {
-        try {
-            has_reachable_storehouse(dynamic_cast<const PackageSender*>(&ramp),&color)
+    //    NodeCollection<Ramp>::iterator it;
+    for (auto &ramp : ramp_collection_)
+    {
+        try
+        {
+            has_reachable_storehouse(dynamic_cast<const PackageSender *>(&ramp), color);
         }
-        catch (std::logic_error){
+        catch (std::logic_error &)
+        {
             return false;
         }
-        return true;
     }
+    return true;
 }
 
-bool Factory::has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors)const{
-    if(node_colors[sender] == NodeColor::VERIFIED)
+bool Factory::has_reachable_storehouse(const PackageSender *sender, std::map<const PackageSender *, NodeColor> &node_colors) const
+{
+    if (node_colors[sender] == NodeColor::VERIFIED)
         return true;
     node_colors[sender] = NodeColor::VISITED;
 
-//    jeśli `nadawca` nie ma zdefiniowanych odbiorców: //FIXME Ktoś ma jakiś pomysł?
-    if(sender->receiver_preferences_.get_preferences().empty()){
+    //    jeśli `nadawca` nie ma zdefiniowanych odbiorców: //FIXME Ktoś ma jakiś pomysł?
+    if (sender->receiver_preferences_.get_preferences().empty())
+    {
         throw std::logic_error("There is no reciver");
     }
     bool has_other_recivers_than_it = false;
-//    for(jeśli typ odbiorcy to MAGAZYN): //FIXME Ktoś ma jakiś pomysł?
-    for(const auto& el:sender->receiver_preferences_.get_preferences()){
-        if(el.first -> get_receiver_type() == ReceiverType::STOREHOUSE) {
+    //    for(jeśli typ odbiorcy to MAGAZYN): //FIXME Ktoś ma jakiś pomysł?
+    for (const auto &el : sender->receiver_preferences_.get_preferences())
+    {
+        if (el.first->get_receiver_type() == ReceiverType::STOREHOUSE)
+        {
             has_other_recivers_than_it = true;
         }
-        else if(el.first -> get_receiver_type() == ReceiverType::WORKER) {
-            IPackageReceiver* receiver_ptr = el.first ;
-            auto worker_ptr = dynamic_cast<Worker*>(receiver_ptr);
-            auto sendrecv_ptr = dynamic_cast<PackageSender*>(worker_ptr);
-            if(sendrecv_ptr==sender){
+        else if (el.first->get_receiver_type() == ReceiverType::WORKER)
+        {
+            IPackageReceiver *receiver_ptr = el.first;
+            auto worker_ptr = dynamic_cast<Worker *>(receiver_ptr);
+            auto sendrecv_ptr = dynamic_cast<PackageSender *>(worker_ptr);
+            if (sendrecv_ptr == sender)
+            {
                 continue;
             }
             has_other_recivers_than_it = true;
-            if(node_colors[sendrecv_ptr]==NodeColor::UNVISITED){
-                has_reachable_storehouse(sendrecv_ptr,node_colors);
+            if (node_colors[sendrecv_ptr] == NodeColor::UNVISITED)
+            {
+                has_reachable_storehouse(sendrecv_ptr, node_colors);
             }
         }
     }
     node_colors[sender] = NodeColor::VERIFIED;
-    if(has_other_recivers_than_it)
+    if (has_other_recivers_than_it)
         return true;
     else
         throw std::logic_error("There is no reciver");
 }
-
