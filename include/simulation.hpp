561b0c218b48070112ac29b943bb7162919a204c
BartoszBartt <bartoszewski@student.agh.edu.pl>
2022-01-16 16:35:16 +0100

    simulation

---
+++ include/simulation.hpp
@@ -0,0 +1,6 @@
+//1b: Bartoszewski (406690), Gajek (400365), Gąsior (407326), Kowalczyk (406185)
+#ifndef NETSIM_SIMULAION_HPP
+#define NETSIM_SIMULAION_HPP
+
+#endif //NETSIM_SIMULAION_HPP
+//1b: Bartoszewski (406690), Gajek (400365), Gąsior (407326), Kowalczyk (406185)
---
+++ src/simulation.cpp
@@ -0,0 +1,3 @@
+//1b: Bartoszewski (406690), Gajek (400365), Gąsior (407326), Kowalczyk (406185)
+
+#include "simulation.hpp"
